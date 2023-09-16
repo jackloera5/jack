32d549169d3a0b5d7e079b642011ba0a504c1534
jackloera5 <144299828+jackloera5@users.noreply.github.com>
2023-09-15 22:29:59 -0700

    Add files via upload
    Reutilizacion de funciones

---
+++ main (22).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int main()
+{
+    int x {};
+    std::cout<< "ingrese un entero:";
+    std::cin>> x ;
+    
+    int y{};
+    std::cout<<"ingrese un entero:";
+    std::cin>> y ;
+    
+    std::cout << x << " + " << y <<" = "<< y+x<<'\n';
+    return 0;
+    
+    
+    
+    
+    
+    
+    
+    
+}
