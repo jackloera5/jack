1b43845067cd7ca3a8d964d8ab6ee74cf0d4dc48
jackloera5 <144299828+jackloera5@users.noreply.github.com>
2023-09-15 22:29:23 -0700

    Add files via upload
    pag27

---
+++ main (21).cpp
@@ -0,0 +1,14 @@
+//int getValvueFromUser()//esta funcio devuelve ahora un valor entero
+//{
+  //  std::cout<<"ingrese un entero:";
+  //  int input{};
+  //  std::cin >>input;
+    
+  //  return input;// devuelve el valor 
+    
+    
+    
+    
+    
+    
+}
