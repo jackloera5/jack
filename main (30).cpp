08517f3568d0f432c952aa4b1b3ec9f7aa18976f
jackloera5 <144299828+jackloera5@users.noreply.github.com>
2023-09-16 19:05:20 -0700

    Add files via upload
    parametros y argumentos de la funcion

---
+++ main (30).cpp
@@ -0,0 +1,17 @@
+// esta funcion no toma parametros 
+// no depende de la persona que llama para nada 
+
+void doprint
+{
+    std::cout << "en doprint ()\n";
+    
+}
+// esta funcion toma un parametro entero llamado x 
+// la persona que llama proporcionara el valor de x 
+void printValue (int x)
+{
+    std::cout << x << '\n';
+    
+}
+//esta funcion tiene dos parametros enteros , uno llamado x y el otro llamado 
+// la persona que llama proporcionara el valor tanto de x como de y 
