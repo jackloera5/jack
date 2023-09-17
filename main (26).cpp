6d259e52a17e5fdb259d464dbc782773f5c09045
jackloera5 <144299828+jackloera5@users.noreply.github.com>
2023-09-16 18:26:12 -0700

    Add files via upload
    pag 36

---
+++ main (26).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void printA()             // define A
+{
+    std::cout <<"A\n";    // //imprime A
+}
+
+void printB()            // define  B 
+{
+    std::cout << "b\n";     //imprime B
+    
+}
+
+int main()
+{
+    printA();              //llama ala funcion
+    printB();             //llama ala funcion
+    
+    return 0;
+}
