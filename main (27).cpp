fdab9b2d4f05fc20c92ab6c896dc94bf5ffef290
jackloera5 <144299828+jackloera5@users.noreply.github.com>
2023-09-16 18:34:27 -0700

    Add files via upload
    pagg 36 codigo corregido ya que como viene esta mal echo

---
+++ main (27).cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+
+//void printA()
+//{
+ //   std::cout << "A\n";      el codigo esta mal echo y seria de la siguiente manera
+    
+//}
+
+//int main()
+//{
+//    std::cout << printA() << '\n';
+    
+//    return 0;
+//}
+#include <iostream>
+
+void printA()
+{
+    std::cout << "A\n";
+}
+
+int main()
+{
+    printA(); // Llama a la función para imprimir "A\n"
+    return 0;
+}
+
