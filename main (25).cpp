d56cf70dd0b0d214756d7b717c46d525adcfb2fe
jackloera5 <144299828+jackloera5@users.noreply.github.com>
2023-09-16 18:05:27 -0700

    Add files via upload
    pag 33

---
+++ main (25).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int return7()
+
+{
+    return 7 ;
+    
+}
+
+int return9()
+{
+    return 9;
+    
+}
+
+int main ()    //l valor de retorno no se almacena ni se muestra ni se utiliza de ninguna manera
+{
+    return7();
+    return9();
+    
+    return 0;
+    
+}
